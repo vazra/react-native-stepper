31113d438c99b9ff59a987f7ea8b3310f440ac1c
Ameen Rashad <ameenrashad@hotmail.com>
2020-08-18 21:44:26 +0530

    feat: added action button

---
+++ packages/react-native-stepper/src/step/ActionButton.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet, TouchableOpacity, GestureResponderEvent } from 'react-native'
+import { StepHeaderView } from './StepHeaderView'
+import { useState } from 'react'
+import { instanceOf, string } from 'prop-types'
+import { useStep } from '../StepProvider'
+
+type ActionButtonProps = {
+  hidden?: boolean
+  style?: { [key: string]: any }
+  title: string
+  color?: string
+  onPress: ((event: GestureResponderEvent) => void) | undefined
+}
+
+const hideStyle = {
+  width: 0,
+  paddingHorizontal: 0,
+}
+
+export const ActionButton = ({ hidden = false, style = {}, color = '#9F9F9F', title, onPress }: ActionButtonProps) => {
+  // occupy space but hide.
+  const defaultStyle = {
+    backgroundColor: color,
+    color: 'white',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 4,
+    elevation: 4,
+    shadowOffset: { width: 5, height: 5 },
+    shadowColor: 'grey',
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
+    marginRight: 16,
+  }
+  const hide = hidden
+    ? {
+        width: 0,
+        paddingHorizontal: 0,
+      }
+    : {}
+  return hidden ? null : (
+    <TouchableOpacity style={{ ...defaultStyle, ...style, ...hide }} disabled={hidden} onPress={onPress}>
+      <Text style={{ color: style.color || defaultStyle.color }}>{title}</Text>
+    </TouchableOpacity>
+  )
+}
